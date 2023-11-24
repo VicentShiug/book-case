09d44af26af4e9db0f2e91e3e8a3b29f5ffff5ca
Guilherme dos santo souza <guilherme.souza@presidencia.gov.br>
2023-11-24 10:15:36 -0300

    feat: profile component

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,30 +1,32 @@
 import { useAuthContext } from '@/context/AuthContext'
-import { getLivros } from '@/hooks/useGetLivros'
-import { auth } from '@/service/firebase'
+import { useLogout } from '@/hooks/useAuth'
+import { ArrowDown } from '@/icons/Icons'
 import Image from 'next/image'
-import { useRouter } from 'next/navigation'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Profile () {
-  const { setLivros, user } = useAuthContext()
-  const router = useRouter()
+  const { user, setUser, setToken } = useAuthContext()
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+  const handleClickDropdown = () => {
+    setDropdownVisible(!isDropdownVisible);
+  };
+
   return (
     <div className='absolute right-0 top-10'>
       <div>
-        <aside className='flex items-center gap-3 border-solid bg-white w-52 h-14 rounded-full p-1 shadow-md mr-10'>
+        <aside className='flex items-center gap-5 border-solid bg-white w-52 h-14 rounded-full p-1 shadow-md mr-10'>
           <Image className='rounded-full shadow-xl'
             src={user?.avatar} width={45} height={45} alt='profileImage' unoptimized />
-          <h1 clas>{user?.name.split(" ")[0]}</h1>
+          <h1 className='text-lg text-gray-800'>{user?.name.split(" ")[0]}</h1>
+          <span className='cursor-pointer ml-auto' onClick={handleClickDropdown}>{<ArrowDown />}</span>
+          <div className='relative'>
+            <ul id='dropdown' className={`absolute right-0 mt-10 p-4 bg-white rounded-xl shadow-md text-gray-700 ${isDropdownVisible ? 'visible' : 'invisible'}`}>
+              <li className='block px-4 py-2 rounded-md hover:bg-gray-100 '>Perfil</li>
+              <li className='block px-4 py-2 rounded-md hover:bg-gray-100 '>Configurações</li>
+              <li onClick={useLogout({ setUser, setToken })} className='block px-4 py-2 rounded-md hover:bg-gray-100 '>Sair</li>
+            </ul>
+          </div>
         </aside>
-        <main>
-          <button onClick={() => {
-            auth.signOut()
-            setUser(null)
-            setToken(null)
-            router.push('/Login')
-          }}>Logout</button>
-          <button onClick={() => getLivros({ user, setLivros })}>pegar livros</button>
-        </main>
       </div>
     </div>
   )
