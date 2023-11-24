d34857e3e85ee0f49541f816598eff98e53e72e5
Guilherme dos santo souza <guilherme.souza@presidencia.gov.br>
2023-11-24 10:17:23 -0300

    refactor: adjust hook useLogout

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,6 +1,19 @@
 import { AuthContext } from '@/context/AuthContext'
+import { auth } from '@/service/firebase'
+import { useRouter } from 'next/navigation'
 import React, { useContext } from 'react'
 
 export default function useAuth () {
   return useContext(AuthContext)
 }
+
+export const useLogout = () => {
+  const { setUser, setToken } = useContext(AuthContext)
+  const router = useRouter()
+  return () => {
+    auth.signOut()
+    router.push('/Login')
+    setUser(null)
+    setToken(null)
+  }
+ }
