c5d49f5305088bff059366af5c462f8007f5b7ef
Guilherme dos santo souza <guilherme.souza@presidencia.gov.br>
2023-12-06 12:10:38 -0300

    feat: remover scroll add flex-wrap

--- src/components/booksList/BooksList.js
+++ src/components/booksList/BooksList.js
@@ -4,16 +4,12 @@ import { useRouter } from 'next/navigation'
 
 export default function BooksList ({ books, className }) {
   const router = useRouter()
-  const ref = React.useRef(null);
-  const scroll = (scrollOffset) => {
-    ref.current.scrollLeft += scrollOffset;
-  };
+
   return (
     <>
-      {
-        books?.length > 0 &&
-        <div ref={ref} className='flex overflow-auto overscroll-auto scroll-smooth relative scrollbar-thin scrollbar-thumb-blue-gray-50 py-10' >
-          <div className={'flex flex-row gap-10 ml-10 mr-10 ' + className}>
+      {books?.length > 0 &&
+        <div className='flex w-full py-10' >
+          <div className={'flex flex-wrap gap-10 ml-10 mr-10 shrink' + className}>
             {
               books?.map(book => {
                 return (
@@ -28,14 +24,6 @@ export default function BooksList ({ books, className }) {
               })
             }
           </div>
-          {/* <div className='overflow-auto'>
-            <button onClick={() => scroll(500)} className='absolute right-0'>
-              setinha para a direita
-            </button>
-            <button onClick={() => scroll(-500)} className=' absolute left-0'>
-              setinha para a esquerda
-            </button>
-          </div> */}
         </div>
       }
     </>
