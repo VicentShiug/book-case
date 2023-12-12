45c6687770f0114dd3b8d80b89c029271d9cd747
Guilherme dos santo souza <guilherme.souza@presidencia.gov.br>
2023-12-12 13:48:08 -0300

    feat: profile component responsiveness

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -14,18 +14,23 @@ export default function Profile () {
   };
 
   return (
-    <div className='absolute top-8 right-3'>
+    <div className='absolute sm:top-8 sm:right-3 top-5 right-2'>
       <div>
-        <aside className='flex items-center gap-5 border-solid bg-white w-52 h-14 rounded-full p-1 border-2 mr-10'>
-          <Image className='rounded-full'
-            src={user?.avatar || '../../jpg/avatar-placeholder.jpg'} width={45} height={45} alt='profileImage' unoptimized />
-          <h1 className='text-lg text-gray-800'>{user?.name.split(" ")[0]}</h1>
+        <aside className='flex items-center sm:gap-5 gap-2 border-solid bg-white sm:w-52 w-32 sm:h-14 rounded-full p-1 border-2 sm:mr-10'>
+          <Image className='rounded-full w-10 h-10 sm:w-12 sm:h-12'
+            src={user?.avatar || '../../jpg/avatar-placeholder.jpg'}
+            width={45}
+            height={45}
+            alt='profileImage'
+            unoptimized
+          />
+          <h1 className='sm:text-lg text-sm text-gray-800'>{user?.name.split(" ")[0]}</h1>
           <span className='cursor-pointer ml-auto' onClick={handleClickDropdown}>{<ArrowDown />}</span>
           <div className='relative'>
-            <ul id='dropdown' className={`absolute right-0 mt-10 p-4 bg-white rounded-xl shadow-md text-gray-700 ${isDropdownVisible ? 'visible' : 'invisible'}`}>
-              <li className='block px-4 py-2 rounded-md hover:bg-gray-100 cursor-pointer'>Perfil</li>
-              <li className='block px-4 py-2 rounded-md hover:bg-gray-100 cursor-pointer'>Configurações</li>
-              <li onClick={useLogout()} className='cursor-pointer block px-4 py-2 rounded-md hover:bg-gray-100 '>Sair</li>
+            <ul id='dropdown' className={`absolute right-0 w-32 sm:w-48 mt-10 p-4 bg-white rounded-xl shadow-md text-gray-700 ${isDropdownVisible ? 'visible' : 'invisible'}`}>
+              <li className='block sm:px-4 py-2 rounded-md hover:bg-gray-100 cursor-pointer'>Perfil</li>
+              <li className='block sm:px-4 py-2 rounded-md hover:bg-gray-100 cursor-pointer'>Configurações</li>
+              <li onClick={useLogout()} className='cursor-pointer block sm:px-4 py-2 rounded-md hover:bg-gray-100 '>Sair</li>
             </ul>
           </div>
         </aside>
