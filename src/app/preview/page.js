20da65434ad3c7937275054159cdca2e74596c89
Guilherme dos santo souza <guilherme.souza@presidencia.gov.br>
2023-12-14 09:01:18 -0300

    feat: preview page responsiveness

--- src/app/preview/page.js
+++ src/app/preview/page.js
@@ -5,6 +5,7 @@ import Profile from '@/components/profile/Profile';
 import Sidebar from '@/components/sidebar/Sidebar';
 import SkeletonPreview from '@/components/skeletonPreview';
 import Statusbar from '@/components/statusbar/Statusbar';
+import TopBar from '@/components/topBar/Topbar';
 import { useAuthContext } from '@/context/AuthContext';
 import { onGetABook } from '@/hooks/useGetBooks';
 import { onAddBookRead, onAddBookReading, onAddBookToRead, onRemoveBookRead, onRemoveBookReading, onRemoveBookToRead } from '@/hooks/useSaveBook';
@@ -96,36 +97,37 @@ export default function Preview () {
   const router = useRouter()
   return (
     <BackgroundArea>
-      <Profile />
-      <Sidebar />
+      <TopBar />
       {
         isLoading && <SkeletonPreview />
       }
       {
         !isLoading &&
-        <div className='w-full h-full pt-40 pl-11 flex gap-20'>
-          <div className='flex flex-col'>
+        <div className='w-full h-full sm:pt-40 pt-8 sm:pl-11 mx-2 flex sm:gap-20 gap-2 flex-wrap sm:flex-nowrap'>
+          <div className='flex flex-col sm:w-auto w-full'>
             <button onClick={() => router.back()} className='flex gap-2 items-center text-gray-700'> {<BackArrowIcon />}Voltar para resultados</button>
-            <div className=' flex flex-col gap-7 w-72 h-96 bg-white rounded-xl mt-5 items-center justify-center'>
-              <div className='!w-52 !h-72'>
-                <Image
-                  alt='book cover'
-                  src={image.replace('http', 'https')}
-                  width={209}
-                  height={277}
-                  unoptimized
-                />
-              </div>
-              <div className='flex gap-4 invisible'>
-                <button>Review</button>
-                <button>Notes</button>
-                <button>Share</button>
+            <div className='flex w-full justify-center sm:justify-start'>
+              <div className='flex flex-col gap-7 sm:w-72 sm:h-96 sm:p-0 p-2 bg-white rounded-xl mt-5 items-center justify-center'>
+                <div className='sm:!w-52 sm:!h-72'>
+                  <Image
+                    alt='book cover'
+                    src={image.replace('http', 'https')}
+                    width={209}
+                    height={277}
+                    unoptimized
+                  />
+                </div>
+                <div className='sm:flex gap-4 hidden'>
+                  <button>Review</button>
+                  <button>Notes</button>
+                  <button>Share</button>
+                </div>
               </div>
             </div>
           </div>
           <div className='flex flex-col gap-7'>
             <div className=' flex flex-col gap-2'>
-              <h1 className='text-4xl leading-10 font-light'>{title}</h1>
+              <h1 className='sm:text-4xl text-2xl leading-10 font-light'>{title}</h1>
               <h2 className='text-sm font-light '>By <span className='underline'>{authors}</span>, {publishedDate?.slice(0, 4)}</h2>
             </div>
             <div className='flex gap-3 items-center '>
@@ -146,23 +148,23 @@ export default function Preview () {
               {/* Disponibilidade e Status */}
               <div className='flex flex-col gap-2'>
                 <p className='font-semibold'>Disponibilidade</p>
-                <Availability saleInfo={saleInfo} />
+                <Availability osaleInfo={saleInfo} />
               </div>
               <div className='flex flex-col gap-2'>
                 <p className='font-semibold'>Status</p>
                 <Statusbar bookId={id} />
               </div>
               {/* Select status livro */}
-              <div className='ml-10 '>
+              <div className=' '>
                 {
                   isLoadingRequest
                     ? <button className='bg-gray-800 text-white rounded-lg px-4 py-2'>Atualizando...</button>
                     :
-                      <select
-                        defaultValue={status || '#'}
-                        value={status}
-                        onChange={(e) => { handleChangeStateBook(e) }}
-                        className="outline-none bg-gray-800 border text-white rounded-lg  block w-full py-2.5 px-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <select
+                      defaultValue={status || '#'}
+                      value={status}
+                      onChange={(e) => { handleChangeStateBook(e) }}
+                      className="outline-none bg-gray-800 border text-white rounded-lg block sm:w-full w-4/5 py-2.5 px-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                       <option value='#'>{status == '#' ? 'Adicionar a Lista' : 'Remover da lista'} </option>
                       <option value="reading">Lendo</option>
                       <option value="toRead">À ler</option>
--- src/components/backgroundArea/BackgroundArea.js
+++ src/components/backgroundArea/BackgroundArea.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function BackgroundArea ({children}) {
   return (
-    <div className="flex flex-col rounded-s-xl relative bg-gray-100 sm:flex-row w-full h-full overflow-auto ">
+    <div className="flex flex-col rounded-s-xl relative bg-gray-100 sm:flex-row w-full h-full sm:overflow-auto overflow-x-hidden">
       {children}
     </div>
   )
